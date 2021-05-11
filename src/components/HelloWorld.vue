4e56e30069df7a4a7a2610825ca7fdbb5f8d72b5
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-05-11 08:59:12 -0500

    Mostrando la directiva IF

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,32 +3,35 @@
     <div class="card-header">
       <div class="d-flex justify-content-between">
         <span>Listado de Usuarios</span>
-        <div class="btn btn-primary">Agregar usuario</div>
+        <div class="btn btn-primary" v-on:click='isVisibleForm(true)'>Agregar usuario</div>
       </div>
       
     </div>
     <div class="card-body">
-      <div class="row">
-        <div class="col-3">
-          <input type="text" class='form-control'>
+      
+      <div v-if='viewForm'>
+        <div class="row">
+          <div class="col-3">
+            <input type="text" class='form-control' placeholder='nombre'>
+          </div>
+          <div class="col-3">
+            <input type="text" class='form-control' placeholder='apellidos'>
+          </div>
+          <div class="col-3">
+            <input type="text" class='form-control' placeholder='telefono'>
+          </div>
+          <div class="col-3">
+            <input type="text" class='form-control' placeholder='correo'>
+          </div>
         </div>
-        <div class="col-3">
-          <input type="text" class='form-control'>
-        </div>
-        <div class="col-3">
-          <input type="text" class='form-control'>
-        </div>
-        <div class="col-3">
-          <input type="text" class='form-control'>
-        </div>
-      </div>
 
-      <div class="row mt-2">
-        <div class="col-6">
-          <div class="btn btn-info">Guardar</div>
-        </div>
-        <div class="col-6">
-          <div class="btn btn-danger">Cancelar</div>
+        <div class="row mt-2">
+          <div class="col-6">
+            <div class="btn btn-info">Guardar</div>
+          </div>
+          <div class="col-6">
+            <div class="btn btn-danger" @click='isVisibleForm(false)'>Cancelar</div>
+          </div>
         </div>
       </div>
 
@@ -59,3 +62,19 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      viewForm: false
+    }
+  },
+  // Dentro de methods se especifican todos los metodos(funciones)
+  methods: {
+    isVisibleForm: function (value){
+      this.viewForm = value
+    }
+  }
+}
+</script>

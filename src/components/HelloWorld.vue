576a96ccf652e549dff225eb0ff79189569c276c
Uziel Trujillo Colon <uziel.trujillo@gmail.com>
2021-05-12 08:56:24 -0500

    Trabajando con v-model 	modified:   src/components/HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,34 +3,33 @@
     <div class="card-header">
       <div class="d-flex justify-content-between">
         <span>Listado de Usuarios</span>
-        <div class="btn btn-primary" v-on:click='isVisibleForm(true)'>Agregar usuario</div>
+        <div class="btn btn-primary" v-on:click='viewForm = true'>Agregar usuario</div>
       </div>
       
     </div>
     <div class="card-body">
-      
       <div v-if='viewForm'>
         <div class="row">
           <div class="col-3">
-            <input type="text" class='form-control' placeholder='nombre'>
+            <input type="text" v-model='user.nombre' class='form-control' placeholder='nombre'>
           </div>
           <div class="col-3">
-            <input type="text" class='form-control' placeholder='apellidos'>
+            <input type="text" v-model='user.apellidos' class='form-control' placeholder='apellidos'>
           </div>
           <div class="col-3">
-            <input type="text" class='form-control' placeholder='telefono'>
+            <input type="text" v-model='user.telefono' class='form-control' placeholder='telefono'>
           </div>
           <div class="col-3">
-            <input type="text" class='form-control' placeholder='correo'>
+            <input type="text" v-model='user.correo' class='form-control' placeholder='correo'>
           </div>
         </div>
 
         <div class="row mt-2">
           <div class="col-6">
-            <div class="btn btn-info">Guardar</div>
+            <div class="btn btn-info" @click='store()'>Guardar</div>
           </div>
           <div class="col-6">
-            <div class="btn btn-danger" @click='isVisibleForm(false)'>Cancelar</div>
+            <div class="btn btn-danger" @click='viewForm = false'>Cancelar</div>
           </div>
         </div>
       </div>
@@ -51,12 +50,6 @@
             <td></td>
             <td></td>
           </tr>
-          <tr>
-            <td>[Editar] [Borrar]</td>
-            <td>Pedro</td>
-            <td></td>
-            <td></td>
-          </tr> 
         </tbody>
       </table>
     </div>
@@ -67,13 +60,20 @@
 export default {
   data () {
     return {
-      viewForm: false
+      viewForm: false,
+      users: [],
+      user: {
+        nombre: '',
+        apellidos: '',
+        telefono: '',
+        correo: ''
+      }
     }
   },
   // Dentro de methods se especifican todos los metodos(funciones)
   methods: {
-    isVisibleForm: function (value){
-      this.viewForm = value
+    store() {
+      console.log( this.user )
     }
   }
 }
